7dd48e5f41cc7c93f7ff980fcd27e1b6a4f9c19b
iori <iori.nishizawa@gmail.com>
2016-10-15 08:06:22 +0900

    new theme

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(".full img").on("click", function() {
-  $(this).toggleClass("zoom");
-});
+// Copyright 2014-2015 Twitter, Inc.
+// Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement('style')
+    msViewportStyle.appendChild(
+        document.createTextNode(
+            '@-ms-viewport{width:auto!important}'
+        )
+    )
+    document.querySelector('head').appendChild(msViewportStyle)
+}
